3a45d9722c2823f18eec99dc6ba3c01115680f43
Andrew Hinh <ajhinh@gmail.com>
2026-08-13 11:52:45 -0700

    fix docs search

--- docs-next/astro.config.mjs
+++ docs-next/astro.config.mjs
@@ -111,6 +111,7 @@ export default defineConfig({
       customCss: ['./src/styles/custom.css'],
       components: {
         Header: './src/components/Header.astro',
+        Search: './src/components/Search.astro',
         Sidebar: './src/components/Sidebar.astro',
         PageSidebar: './src/components/PageSidebar.astro',
         PageTitle: './src/components/PageTitle.astro',
